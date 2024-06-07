0dc0b4391f47a8451fe87deec49e45baa4af4cef
Hanveshit <hanveshithreddy@gmail.com>
2024-06-07 14:36:07 +0530

    Added /notify route

--- Backend/Routes/BookRouter.js
+++ Backend/Routes/BookRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {AddBook,GetBook,GetBooks} = require("../Controllers/BookController");
+const {AddBook,GetBook,GetBooks,Notify} = require("../Controllers/BookController");
 const route = express.Router();
+const {authenticateUser} = require('../middleware')
 
 
 route.post("/",AddBook);
 route.get("/",GetBooks);
 route.get("/book/:id",GetBook);
+route.post("/book/notify/:id",authenticateUser,Notify);
 
 module.exports = route;
