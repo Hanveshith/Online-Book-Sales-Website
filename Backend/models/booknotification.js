583e31e9d47d184f929136795f9bdd0d62927727
Hanveshit <hanveshithreddy@gmail.com>
2024-06-07 14:35:13 +0530

    updated default id of booknotification table to auto

--- Backend/models/booknotification.js
+++ Backend/models/booknotification.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookNotificationSchema = new mongoose.Schema({
-  id: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId() },
+  id: { type: mongoose.Schema.Types.ObjectId, default: mongoose.Types.ObjectId,auto:true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
 });
